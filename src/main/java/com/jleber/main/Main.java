4a56871b53598574f8f80ddbca04a24aeabbef60
João Leber <joao.leber91@gmail.com>
2017-06-20 19:33:51 -0300

    Adding jaxb stuff

---
+++ src/main/java/com/jleber/main/Main.java
@@ -0,0 +1,13 @@
+package com.jleber.main;
+
+import com.jleber.nfeschema.TEnviNFe;
+
+/**
+ * Created by jleber on 20/06/17.
+ */
+public class Main {
+    public static void main(String[] args){
+        TEnviNFe tEnviNFe = new TEnviNFe();
+        tEnviNFe.setVersao("1.0.0");
+    }
+}
