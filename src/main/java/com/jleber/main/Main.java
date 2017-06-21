b9a716fe22780d7407062c16781f2b2ef878eaed
João Leber <joao.leber91@gmail.com>
2017-06-21 12:22:37 -0300

    Fix for exception

--- src/main/java/com/jleber/main/Main.java
+++ src/main/java/com/jleber/main/Main.java
@@ -1,8 +1,11 @@
 package com.jleber.main;
 
+import com.jleber.nfeschema.ObjectFactory;
 import com.jleber.nfeschema.TEnviNFe;
+import com.jleber.nfeschema.TNFe;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.File;
@@ -12,18 +15,27 @@ import java.io.File;
  */
 public class Main {
     public static void main(String[] args) throws JAXBException {
-        TEnviNFe tEnviNFe = new TEnviNFe();
-        tEnviNFe.setVersao("1.0.0");
+
 
         File file = new File("sefaz.xml");
         JAXBContext jaxbContext = JAXBContext.newInstance(TEnviNFe.class);
         Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
 
+        TNFe.InfNFe infNFe = new TNFe.InfNFe();
+        infNFe.setVersao("versao");
+        infNFe.setId("id 1");
+
+        TNFe nfe = new TNFe();
+        nfe.setInfNFe(infNFe);
+
+        ObjectFactory factory = new ObjectFactory();
+        JAXBElement<TNFe> nFeJaxb = factory.createNFe(nfe);
+
         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-        jaxbMarshaller.marshal(tEnviNFe, file);
-        jaxbMarshaller.marshal(tEnviNFe, System.out);
+        jaxbMarshaller.marshal(nFeJaxb, file);
+        jaxbMarshaller.marshal(nFeJaxb, System.out);
 
     }
 }
