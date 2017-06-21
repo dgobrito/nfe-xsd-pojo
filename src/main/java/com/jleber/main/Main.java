e8e3d73bcdbb36d7aeacbf42f55eda76d5601351
João Leber <joao.leber91@gmail.com>
2017-06-21 11:49:36 -0300

    Adding mashall example

--- src/main/java/com/jleber/main/Main.java
+++ src/main/java/com/jleber/main/Main.java
@@ -2,12 +2,28 @@ package com.jleber.main;
 
 import com.jleber.nfeschema.TEnviNFe;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+
 /**
  * Created by jleber on 20/06/17.
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws JAXBException {
         TEnviNFe tEnviNFe = new TEnviNFe();
         tEnviNFe.setVersao("1.0.0");
+
+        File file = new File("sefaz.xml");
+        JAXBContext jaxbContext = JAXBContext.newInstance(TEnviNFe.class);
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        jaxbMarshaller.marshal(tEnviNFe, file);
+        jaxbMarshaller.marshal(tEnviNFe, System.out);
+
     }
 }
